bcaa76638edae44c86149a3a686cf98266d6504f
Contract Addresses Updater <you@example.com>
2023-08-17 10:08:35 -0400

    Add token contract preview 10

---
+++ src/getInfoXLM.js
@@ -0,0 +1,18 @@
+import StellarSdk from "stellar-sdk";
+import settings from "../settings.json"  assert { type: "json" };
+
+const args = process.argv;
+const user_address = args[2]
+
+var server = new StellarSdk.Server(settings.horizonUrl, {allowHttp: true});
+
+server.loadAccount(user_address)
+  .then(account => {
+    // Find the XLM balance
+    const xlmBalance = account.balances.find(balance => balance.asset_type === 'native');
+
+    console.log(`Your XLM balance: ${xlmBalance.balance}`);
+  })
+  .catch(error => {
+    console.error('Error loading account:', error);
+  });
