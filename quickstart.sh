9d0842e10f54aa7ab8dcead605e9e731836d5dba
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-12-28 10:05:15 -0300

    use protocol 20

--- quickstart.sh
+++ quickstart.sh
@@ -1,55 +1,65 @@
 #!/bin/bash
 
+previewVersion=$(jq -r '.previewVersion' preview_version.json)
+quickstartHash=$(jq -r '.quickstartHash' preview_version.json)
+ 
 set -e
 
 case "$1" in
 standalone)
     echo "Using standalone network"
-    ARGS="--standalone"
+    ARGS="--local --enable-soroban-diagnostic-events"
     ;;
 futurenet)
     echo "Using Futurenet network"
     ARGS="--futurenet"
     ;;
+testnet)
+    echo "Using Testnet network"
+    ARGS="--testnet"
+    ;;
 *)
-    echo "Usage: $0 standalone|futurenet"
+    echo "Usage: $0 standalone|futurenet|testnet"
     exit 1
     ;;
 esac
 
-# Run the soroban-preview container
-# Remember to do 
-# cd docker
-# bash build.sh
+shift
 
-echo "Searching for a previous soroban-preview-10 docker container"
-containerID=$(docker ps --filter="name=soroban-preview" --all --quiet)
+echo "1. Creating docker soroban network"
+(docker network inspect soroban-network -f '{{.Id}}' 2>/dev/null) \
+  || docker network create soroban-network
+
+
+echo "2. Running a soroban-precview docker container"
+
+echo "Searching for a previous soroban-preview docker container"
+containerID=$(docker ps --filter=`name=soroban-preview-${previewVersion}` --all --quiet)
 if [[ ${containerID} ]]; then
-    echo "Start removing soroban-preview-10  container."
-    docker rm --force soroban-preview-10
-    echo "Finished removing soroban-preview-10 container."
+    echo "Start removing soroban-preview-${previewVersion}  container."
+    docker rm --force soroban-preview-${previewVersion}
+    echo "Finished removing soroban-preview-${previewVersion} container."
 else
-    echo "No previous soroban-preview-10 container was found"
+    echo "No previous soroban-preview-${previewVersion} container was found"
 fi
 
 currentDir=$(pwd)
-docker run --volume  ${currentDir}:/workspace \
-           --name soroban-preview-10 \
-           --interactive \
-           --tty \
-           -p 8001:8000 \
-           --detach \
-           --ipc=host \
-           --network soroban-network \
-           esteblock/soroban-preview:10
+docker run -dti \
+  --volume ${currentDir}:/workspace \
+  --name soroban-preview-${previewVersion} \
+  -p 8001:8000 \
+  --ipc=host \
+  --network soroban-network \
+  esteblock/soroban-preview:${previewVersion}
 
 # Run the stellar quickstart image
 docker run --rm -ti \
-  --platform linux/amd64 \
   --name stellar \
   --network soroban-network \
   -p 8000:8000 \
-  stellar/quickstart:soroban-dev@sha256:8a99332f834ca82e3ac1418143736af59b5288e792d1c4278d6c547c6ed8da3b \
+  stellar/quickstart:${quickstartHash} \
   $ARGS \
   --enable-soroban-rpc \
-  --protocol-version 20 
+  --protocol-version 20 \
+  --enable-soroban-diagnostic-events \
+  "$@" # Pass through args from the CLI
